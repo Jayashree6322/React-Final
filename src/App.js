589070f822b23fed8be2571955f2b02d6f083c75
Jayashree6322 <jayashree06032002@gmail.com>
2025-05-28 16:58:12 +0530

    the solution is.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,7 @@ const App = ({ state: { count, refreshID }}) =>
 		<div>Refresh ID: { refreshID }</div>
 		<div>Hello All</div>
 		<div>Welcome to my page</div>
-		<p><span>
-			<div>1</div>
-			<div>2</div>
-			<div>3</div>
-			<div>4</div>
-			<div>5</div>
-			<div>6</div>
-			<div>7</div>
-			<div>8</div>
-			<div>9</div>
-			<div>10</div>
-		</span></p>
+
 	</div>;
 
 export default App;
